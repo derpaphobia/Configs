98c5c5f57459b2e8f70563f41aa944ff65196a73
derpaphobia <47162801+derpaphobia@users.noreply.github.com>
2019-12-12 23:22:23 +0100

    Update Centos8restore.sh

--- Centos8restore.sh
+++ Centos8restore.sh
@@ -28,8 +28,23 @@ sudo chmod +x /usr/local/bin/docker-compose
 # Fixing/mapping/formattings drives
 ###
 
+mkdir media nextcloud
+sudo mkfs.xfs -f /dev/sda
+sudo mkfs.xfs -f /dev/sdb
+sudo mount /dev/sda /home/derpa/nextcloud
+sudo mount /dev/sdb /home/derpa/media
+sudo chown derpa:derpa /home/derpa/nextcloud
+sudo chown derpa:derpa /home/derpa/media
+sudo chmod 777 /home/derpa/nextcloud
+sudo chmod 777 /home/derpa/media
+sudo umask=777 /home/derpa/nextcloud
+sudo umask=777 /home/derpa/media
+
 sdbuuid=$(sudo blkid -s UUID -o value /dev/sdb)
 sdauuid=$(sudo blkid -s UUID -o value /dev/sda)
+
+echo UUID=$sdauuid  /home/derpa/nextcloud     xfs   defaults 0  0 | sudo tee /etc/fstab -a >/dev/null 2>&1
+echo UUID=$sdbuuid  /home/derpa/media     xfs   defaults 0  0 | sudo tee /etc/fstab -a >/dev/null 2>&1
 
 sleep 1
 
