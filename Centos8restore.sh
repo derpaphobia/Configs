4aa68526c5603bfdc48f8d138f01ea1ff93effb8
derpaphobia <47162801+derpaphobia@users.noreply.github.com>
2019-12-13 00:02:37 +0100

    Update Centos8restore.sh

--- Centos8restore.sh
+++ Centos8restore.sh
@@ -13,19 +13,14 @@ fi
 
 sudo dnf upgrade -y --refresh
 
+###
+# Fixing grub entries for stupid MSI mobo
+###
+sudo sed -i 's/swap rhgb quiet/swap rhgb quiet radeon.dpm=0/' /etc/default/grub
+sudo grub2-mkconfig -o /boot/efi/EFI/centos/grub.cfg
 
 ###
-# Installing apps..
-###
-
-sudo curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
-sudo dnf install -y docker nano epel-release wireguard-dkms wireguard-tools
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-###
-# Fixing/mapping/formattings drives
+# Fixing/mapping/formatting drives
 ###
 
 mkdir media nextcloud
@@ -48,27 +43,12 @@ echo UUID=$sdbuuid  /home/derpa/media     xfs   defaults 0  0 | sudo tee /etc/fs
 
 sleep 1
 
-
-# Adding swap to boot options
-offset=$(sudo swap-offset /swapfile | grep -o 'resume offset = .*' | cut -d" " -f4-)
-uuid=$(sudo blkid -s UUID -o value /dev/sda3)
-sudo kernelstub -a "resume=UUID=$uuid resume_offset=$offset resumedelay=15"
-sudo touch /etc/initramfs-tools/conf.d/resume
-echo "RESUME=UUID=$uuid resume_offset=$offset" | sudo tee /etc/initramfs-tools/conf.d/resume > /dev/null
-sudo update-initramfs -u -k all
-
-
 ###
-# Cleaning apt cache and removing /Configs folder
+# Installing apps..
 ###
 
-sudo apt-get clean
-sudo rm -r ~/Configs
+sudo curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
+sudo dnf install -y docker nano epel-release wireguard-dkms wireguard-tools
 
-
-###
-# Installing oh-my-zsh and changing shell to zsh
-###
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" && chsh -s $(which zsh)
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
