a158bfc863717370187e614562d3c7ada4c7c9ca
derpaphobia <47162801+derpaphobia@users.noreply.github.com>
2019-12-12 21:23:14 +0100

    Update Centos8restore.sh

--- Centos8restore.sh
+++ Centos8restore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $(id -u) = 0 ]; then
-   echo "This script changes your users gsettings and should thus not be run as root!"
+   echo "NO ROOT PLEASE! Also..."
    echo "You may need to enter your password multiple times!"
    exit 1
 fi
@@ -11,17 +11,20 @@ fi
 # Updating
 ###
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
+sudo dnf upgrade -y --refresh
 
 
 ###
-# Installing apps and adding repos..
+# Installing apps..
 ###
 
-sudo add-apt-repository -y ppa:andreasbutti/xournalpp-master
-sudo apt-get -yqq install build-essential cmake qt5-default wget libxtst-dev libxinerama-dev libice-dev libxrandr-dev libavahi-compat-libdnssd-dev libcurl4-openssl-dev libssl-dev dh-make gnome-tweak-tool curl wget flatpak gnome-software-plugin-flatpak snapd exfat-utils ffmpeg gimp gimp-plugin-registry gnome-shell-extension-appindicator htop inkscape krita mpv nautilus-image-converter p7zip papirus-icon-theme tilix gitg nano zsh zsh-syntax-highlighting fortune-mod nautilus-nextcloud steam lutris fonts-firacode xournalpp gnome-shell-extension-no-annoyance barrier network-manager-openvpn-gnome discord nautilus-admin uswsusp php nodejs npm network-manager libnss3-tools jq xsel php-pear php7.2-dev php7.2-curl php7.2-zip php7.2-sqlite3 php7.2-mysql php7.2-pgsql libmcrypt-dev libreadline-dev cifs-utils mariadb-server spotify-client code glances
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo curl -Lo /etc/yum.repos.d/wireguard.repo https://copr.fedorainfracloud.org/coprs/jdoss/wireguard/repo/epel-7/jdoss-wireguard-epel-7.repo
+sudo dnf install -y docker nano epel-release wireguard-dkms wireguard-tools
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+
 
 sleep 1
 
